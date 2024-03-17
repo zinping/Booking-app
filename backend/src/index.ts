e5d768309d06a8f4e976a8f41628863a8311e455
zinping <haozinping@gmail.com>
2024-03-18 01:30:54 +0530

    registration backend complete

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,8 @@
-import express, { Request, Response } from "express";
+import express from "express";
 import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
+import userRoutes from "./routes/users"
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string)
     .then(() => {
@@ -16,11 +17,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-app.get("/api/text", async (req: Request, res: Response) => {
-    res.json({ message: "Hello MERN" });
-});
-
-const PORT = process.env.PORT || 3000;
+app.use("/api/users", userRoutes)
+const PORT = process.env.PORT || 7000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
---
+++ backend/src/routes/users.ts
@@ -0,0 +1,48 @@
+import express, { Request, Response } from "express";
+import User from "../models/user";
+import jwt from "jsonwebtoken";
+import { check, validationResult } from "express-validator";
+
+
+const router = express.Router();
+
+router.post("/register",[
+    check("firstName", "First Name is required").isString(),
+    check("lastName", "Last Name is required").isString(),
+    check("email", "Email is required").isEmail(),
+    check("password","Password with 6 or more characters required").isLength({min: 6})
+], async (req: Request, res: Response) => {
+    try {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+        let user = await User.findOne({
+            email: req.body.email,
+        });
+        if (user) {
+            return res.status(400).json({ message: "User already exists" });
+        }
+
+        user = new User(req.body);
+        await user.save();
+
+        const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET_KEY as string, {
+            expiresIn: "1d"
+        });
+
+        res.cookie("auth_token", token, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === "production",
+            maxAge:  864000000,        
+        })
+        return res.sendStatus(200);
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Server Error" });
+    }
+});
+
+export default router;
