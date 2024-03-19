6b2cb943ea840b1117b66b6f30549eb6d690435b
zinping <haozinping@gmail.com>
2024-03-19 22:19:47 +0530

    commit changes

--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -3,41 +3,67 @@ import { check, validationResult } from "express-validator";
 import User from "../models/user";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
+// import verifyToken from "../middleware/auth";
 
-const router = express.Router()
+const router = express.Router();
 
-router.post("/login",
-    [
-        check("email", "Email is requred").isEmail(),
-        check("password", "Password with 6 or more characters are required").isLength({ min: 6, }),
-    ], async (req: Request, res: Response) => {
-        const errors = validationResult(req)
-        if (!errors.isEmpty()) { return res.status(400).json({ message: errors.array() }) }
+router.post(
+  "/login",
+  [
+    check("email", "Email is required").isEmail(),
+    check("password", "Password with 6 or more characters required").isLength({
+      min: 5,
+    }),
+  ],
+  async (req: Request, res: Response) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ message: errors.array() });
+    }
 
-        const {email, passord}= req.body;
+    const { email, password } = req.body;
 
-        try {
+    try {
+      const user = await User.findOne({ email });
+      if (!user) {
+        return res.status(400).json({ message: "Invalid Credentials" });
+      }
 
-            const user = await User.findOne({ email });
-            if (!user) { return res.status(500).json({ message: "Invalid Credentials" }); }
-            const isMatch = await bcrypt.compare(passord, user.password)
-            if (!isMatch) { return res.status(500).json({ message: "Invalid Credentials" }); }
+      const isMatch = await bcrypt.compare(password, user.password);
+      if (!isMatch) {
+        return res.status(400).json({ message: "Invalid Credentials" });
+      }
 
-            const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET_KEY as string, { expiresIn: "1d" });
-
-            res.cookie("auth token", token, {
-                httpOnly: true,
-                secure: process.env.NODE_ENV === "production",
-                maxAge: 864000000,
-            });
-
-            res.status(200).json({ userId: user._id });
-
-
-        } catch (error) {
-            console.log(error);
-            res.status(500).json({ message: "something went wrong" });
+      const token = jwt.sign(
+        { userId: user.id },
+        process.env.JWT_SECRET_KEY as string,
+        {
+          expiresIn: "1d",
         }
-    });
+      );
+
+      res.cookie("auth_token", token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        maxAge: 86400000,
+      });
+      res.status(200).json({ userId: user._id });
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ message: "Something went wrong" });
+    }
+  }
+);
+
+// router.get("/validate-token", verifyToken, (req: Request, res: Response) => {
+//   res.status(200).send({ userId: req.userId });
+// });
+
+// router.post("/logout", (req: Request, res: Response) => {
+//   res.cookie("auth_token", "", {
+//     expires: new Date(0),
+//   });
+//   res.send();
+// });
 
 export default router;
--- backend/src/routes/users.ts
+++ backend/src/routes/users.ts
@@ -10,7 +10,7 @@ router.post("/register",[
     check("firstName", "First Name is required").isString(),
     check("lastName", "Last Name is required").isString(),
     check("email", "Email is required").isEmail(),
-    check("password","Password with 6 or more characters required").isLength({min: 6})
+    check("password","Password with 6 or more characters required").isLength({min: 5})
 ], async (req: Request, res: Response) => {
     try {
         const errors = validationResult(req);
